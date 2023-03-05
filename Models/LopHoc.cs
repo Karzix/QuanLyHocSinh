e67f153242155b70eb4955e09bcfcf5017b7385c
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-03-05 17:01:39 +0700

    thêm ràng buộc

--- Models/LopHoc.cs
+++ Models/LopHoc.cs
@@ -1,9 +1,17 @@
-﻿namespace WebApplication3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
 {
     public class LopHoc
     {
         public Guid Id { get; set; }
+        [Required]
+        [Display(Name = "Mã lớp học")]
         public string MaLopHoc { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name ="Tên lớp")]
         public string Name { get; set; }
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication3.Models
 {
     public class Student
     {
         public Guid Id { get; set; }
+        [RegularExpression(@"^[0-9]+[0-9\s]*$")]
+        [Required]
+        [Display(Name= "Mã số sinh viên")]
         public string Mssv { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Họ và tên")]
         public string Name { get; set; }
 
         [ForeignKey("LopHocId")]
